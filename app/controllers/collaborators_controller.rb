2fcd62829fed7f0d3642b5e755ed5537bd730792
Gerald Oliva <ryumaster001@yahoo.com>
2016-08-09 11:45:02 -0400

    Fix collaborators bug

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,9 +1,23 @@
 class CollaboratorsController < ApplicationController
   def create
-    @collaborator = Collaborator.new(collab_params)
-    @collaborator.save
 
-    redirect_to @collaborator.wiki
+    new_user_ids = params[:collaborator][:user_id]
+
+
+    new_user_ids.each do |id|
+      break if id.blank?
+
+      Collaborator.create(
+        wiki_id: params[:collaborator][:wiki_id],
+        user_id: id
+      )
+    end
+
+    # @collaborator = Collaborator.new(collab_params)
+    # @collaborator.save
+
+    # redirect_to @collaborator.wiki
+    redirect_to :back
   end
 
   def destroy
@@ -12,7 +26,7 @@ class CollaboratorsController < ApplicationController
     redirect_to @collaborator.wiki
   end
 
-  def collab_params
-    params.require(:collaborator).permit(:wiki_id, :user_id)
-  end
+  # def collab_params
+  #   params.require(:collaborator).permit(:wiki_id, :user_id)
+  # end
 end
